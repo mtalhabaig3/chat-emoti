0cb1603b40613212f069735fff68a56c7fea6168
mtalhabaig3 <mtalhabaig3@gmail.com>
2022-09-06 12:21:50 +0500

    modified Account screen

--- screens/AccountInfo.js
+++ screens/AccountInfo.js
@@ -1,67 +1,155 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { View, SafeAreaView, StyleSheet, Image } from "react-native";
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from "react-native-paper";
+import BackButton from "../components/BackButton";
+
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import { auth } from "../firebase";
 import { SignOut } from "../firebase";
-import { Button } from "react-native-paper";
 
-export default function AccountScreen() {
+const AccountInfo = ({ navigation }) => {
   const { currentUser } = auth;
   return (
-    <View style={styles.container}>
-      <Image style={styles.img} source={{ uri: currentUser.photoURL }} />
-      <Text style={[styles.text, { position: "absolute", top: 240 }]}>
-        {currentUser.displayName}
-      </Text>
-      <Text style={[styles.text, { position: "absolute", top: 340 }]}>
-        This is my Bio! I love Chatemoti
-      </Text>
-
-      <View style={{ flexDirection: "row", position: "absolute", bottom: 120 }}>
-        <Ionicons name="mail" size={30} color="maroon" />
-        <Text
+    <>
+      <SafeAreaView style={styles.container}>
+        <View
           style={[
-            styles.text,
-            { marginLeft: 10, fontSize: 20, fontStyle: "italic" },
+            styles.userInfoSection,
+            { justifyContent: "center", alignItems: "center" },
           ]}
         >
-          {currentUser.email}
-        </Text>
-      </View>
-      <Button mode="contained" style={styles.btn} onPress={SignOut}>
-        Log Out
-      </Button>
-    </View>
+          <View style={{ marginTop: 15 }}>
+            <Image
+              style={styles.image}
+              source={{ uri: currentUser.photoURL }}
+            />
+            <View style={{ marginLeft: 20 }}>
+              <Title
+                style={[
+                  styles.title,
+                  {
+                    marginTop: 15,
+                    marginBottom: 5,
+                  },
+                ]}
+              >
+                {currentUser.displayName}
+              </Title>
+            </View>
+          </View>
+        </View>
+        <View style={styles.userInfoSection}>
+          <View style={styles.row}>
+            <MaterialCommunityIcons
+              name="map-marker-radius"
+              color="maroon"
+              size={20}
+            />
+            <Text style={{ color: "#777777", marginLeft: 20 }}>
+              Tokyo, Japan
+            </Text>
+          </View>
+          <View style={styles.row}>
+            <MaterialCommunityIcons name="phone" color="maroon" size={20} />
+            <Text style={{ color: "#777777", marginLeft: 20 }}>
+              +92-1234567890
+            </Text>
+          </View>
+          <View style={styles.row}>
+            <MaterialCommunityIcons name="email" color="maroon" size={20} />
+            <Text style={{ color: "#777777", marginLeft: 20 }}>
+              {currentUser.email}
+            </Text>
+          </View>
+        </View>
+
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Ionicons name="settings-outline" color="maroon" size={25} />
+            <Text style={styles.menuItemText}>Settings</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple style={styles.btn} onPress={SignOut}>
+          <Text style={styles.btnTxt}>Log Out</Text>
+        </TouchableRipple>
+      </SafeAreaView>
+    </>
   );
-}
+};
+
+export default AccountInfo;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#EADDCA",
+  },
+  userInfoSection: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: "500",
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  infoBoxWrapper: {
+    borderBottomColor: "#dddddd",
+    borderBottomWidth: 1,
+    borderTopColor: "#dddddd",
+    borderTopWidth: 1,
+    flexDirection: "row",
+    height: 100,
+  },
+  infoBox: {
+    width: "50%",
     alignItems: "center",
     justifyContent: "center",
   },
-  img: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    borderWidth: 3,
-    borderColor: "maroon",
-    position: "absolute",
-    top: 30,
+  menuWrapper: {
+    marginTop: 10,
   },
-  text: {
-    fontSize: 28,
-    color: "maroon",
-    fontStyle:'italic'
+  menuItem: {
+    flexDirection: "row",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
   },
   btn: {
-    borderColor: "maroon",
-    borderWidth: 3,
+    height: 50,
+    width: 270,
     backgroundColor: "maroon",
+    borderRadius: 80,
+    marginLeft: 65,
+    justifyContent: "center",
+    alignItems: "center",
     position: "absolute",
-    bottom: 50,
-    color: "maroon",
+    bottom: 250,
   },
+  btnTxt: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 17,
+  },
+  menuItemText: {
+    color: "#777777",
+    marginLeft: 20,
+    fontWeight: "600",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  text: { color: "maroon", marginLeft: 20 },
+  image: { width: 100, height: 100, borderRadius: 50 },
 });
